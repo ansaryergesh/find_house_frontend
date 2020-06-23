1951f2c8957e70b74f80f7e2181987d96af0ee34
ansaryergesh <yergeshovansar@gmail.com>
2020-06-23 13:41:34 +0600

    loader js

--- src/actions/houses.js
+++ src/actions/houses.js
@@ -1,8 +1,8 @@
+import addHouse from "../components/addHouse";
+
 export const houseLoading = () => ({
   type: 'HOUSES_LOADING',
 })
-
-
 
 export const housesFailed = errmess => ({
     type: 'HOUSES_FAILED',
@@ -48,4 +48,32 @@ export const postHouse = (name,description,price)=> {
         .then(response=>dispatch(postHouseSuccess(response)))
           .catch(r => r.json().then(e => dispatch({ type: 'HOUSES_POST_FAILED', payload: e.message })))
     }
+}
+
+export const fetchHouses = () => dispatch => {
+    dispatch(houseLoading(true));
+    return fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/homes`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('jwt')}`
+        }
+    })
+    .then(response=> {
+        if(response.ok) {
+            return response;
+        }
+
+        const error = new Error(`Error ${response.status}: ${response.statusText}`);
+        error.response =response;
+        throw error;
+    },
+    error=> {
+        const errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(homes =>dispatch(addHouse(homes)))
+    .catch(error=> dispatch(housesFailed(error.message)));
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+export const Loading = () => (
+  <div>
+    <Segment>
+      <Dimmer active inverted>
+        <Loader size='large'>Loading</Loader>
+      </Dimmer>
+
+      <Image src='https://react.semantic-ui.comhttps://react.semantic-ui.com/images/wireframe/paragraph.png' />
+    </Segment>
+  </div>
+);
